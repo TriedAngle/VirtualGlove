fa31599737f3b5987e20a2da15f59fd95f3912d7
Sebastian Strobl <Sebastian@Strobl.net>
2019-06-25 07:08:01 +0200

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,18 @@ void loop() {
   getGyroRotation(rotation[0], rotation[1], rotation[2]);
   getAccForce(force[0], force[1], force[2]);
   getAngles(finger[0], finger[1], finger[2], finger[3], finger[4]);
+  float allData[10] = {rotation[0], rotation[1], rotation[2], force[0], force[1], force[2], finger[0], finger[1], finger[2], finger[3]};
 
-  Serial.println(String(finger[0]) + "|" + String(finger[1]) + "|"
-  + String(finger[2]) + "|" + String(finger[3]) + "|" + String(rotation[0]) + "|"
-  + String(rotation[1]) + "|" + String(rotation[2]) + "|" + String(force[0])+ "|"
-  + String(force[1]) + "|" + String(force[2]));
+  Serial.println(finger[1]);
+
+  // Serial.println(String(finger[0]) + "|" + String(finger[1]) + "|"
+  // + String(finger[2]) + "|" + String(finger[3]) + "|" + String(rotation[0]) + "|"
+  // + String(rotation[1]) + "|" + String(rotation[2]) + "|" + String(force[0])+ "|"
+  // + String(force[1]) + "|" + String(force[2]));
 
   //for(int i = 0; i < 5; i++){
   //  Serial.print(finger[i]);
   //  Serial.print(" | ");
   //}
-
-  Serial.println();
-  delay(500);
+  delay(50);
 }
